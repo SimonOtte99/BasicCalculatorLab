9c9fdc611d4cc145b2264b6cbb10c45f87cf615d
SimonOtte99 <59981822+SimonOtte99@users.noreply.github.com>
2020-01-28 16:00:43 -0600

    New Fomulas

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -8,17 +8,27 @@ namespace BasicCalculator
         {
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter the temperature in degrees Fahrenheit");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int dF = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the relative humidity");
+            int rh = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            int dP = dF-9*(100-rh)/25;
 
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("The dew point is " + dP);
+            
+            Console.WriteLine("Enter the windspeed");
+            int ws = int.Parse(Console.ReadLine());
+
+
+            double wc = 35.74 
+                + 0.6215 * dF 
+                - 35.75 * Math.Pow(ws, 0.16) 
+                + 0.4275*dF * Math.Pow(ws, 0.16);
+            Console.WriteLine("The wind chill is " + wc);
         }
     }
 }
